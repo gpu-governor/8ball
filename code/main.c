438f697805a58b211380d9b8bc706b439c42491a
cpu governor <cpu.governor@gmail.com>
2024-11-04 21:07:54 +0100

    taking a break

--- code/main.c
+++ code/main.c
@@ -1,63 +1,94 @@
 #include"raylib.h"
 #include<stdio.h>
-// ============================= Global variables  ======================================
+#include"game.h"
+const int width = 1150;
+const int height = 720;
+#define BALL_COUNT 17
+#define ROW_SPACING 40
+#define BALL_SPACING 40
 
-const int width = 800;
-const int height = 600;
-#define BALL_COUNT 16
-
-typedef struct{
-	int x;
-	int y;
-	Texture2D texture;
-	float scale;
-	char * file_path;
-}Asset;
-
-
-void init();
-void quit();
-Asset load_asset(char * file_path, int x, int y, float scale);
-void render_asset(Asset new_asset);
 // =============================MAIN FUNCTION ======================================
 
 int main(){
-	init();
-	Asset table = load_asset("Assets/table.png", 0,0,0.13f);
+	init(width,height);
+	Asset table = load_asset("Assets/table.png", 20,20,0.24f);
 	Asset cue = load_asset("Assets/cue.png", 150, 150, 0.2f);
 	Asset triangle = load_asset("Assets/triangle.png", 400, 150, 0.2f);
-
 	// balls
 	Asset balls[BALL_COUNT];
 
-	// char filepath[32];
-// 
-// 	for (int i = 0; i <BALL_COUNT; i++){
-// 		sprintf(filepath, "Assets/ball_%d.png", i+1);
-// 		balls[i] = load_asset(filepath, 0,0,0.2f);
-// 	}
+	int offsetX =250;
+	int offsetY = 300;
 
-	balls[1] = load_asset("Assets/ball_1.png", 0, 50, 0.2f);
-	balls[2] = load_asset("Assets/ball_2.png", 10, 50, 0.2f);
-	balls[3] = load_asset("Assets/ball_3.png", 20, 50, 0.2f);
-	balls[4] = load_asset("Assets/ball_4.png", 30, 50, 0.2f);
-	balls[5] = load_asset("Assets/ball_5.png", 40, 50, 0.2f);
-	balls[6] = load_asset("Assets/ball_6.png", 50, 50, 0.2f);
-	balls[7] = load_asset("Assets/ball_7.png", 60, 50, 0.2f);
-	
+	int ballIndex = 1;
+
+	for (int row =5; row>=1; row--){
+		int ballsInRow =row;
+		int startY = offsetY - (BALL_SPACING * (ballsInRow -1))/2; // centers each row vertically
+		int x = offsetX + ROW_SPACING * (5 - row); 
+		for (int i = 0; i <ballsInRow; i++){
+			int y = startY+ i * BALL_SPACING;
+
+			// create filename
+			char filename[50];
+			sprintf(filename,"Assets/ball_%d.png",ballIndex);
+
+			// load asset function
+			balls[ballIndex]= load_asset(filename, x, y, 0.3f);
+
+			ballIndex++;
+		}
+		
+	}
+
+	// white ball
+	balls[16] = load_asset("Assets/ball_16.png", 800,300, 0.3f);
+
+	// collision rectangles walls
+	Rectangle topleft = {110,20, 420, 70};
+	Rectangle topright = {600,20, 420, 70};
+	Rectangle bottomleft = {110,567, 420, 70};
+	Rectangle bottomright = {600,567, 420, 70};	
+	Rectangle sideright = {20,110, 70, 420};
+	Rectangle sideleft = {1040,110,70,420};
+
+	// collisions rectangles pockets hole
+	Rectangle topleftpocket = {20,20,80,80};
+	Rectangle toprightpocket = {1040,20,80,80};
+	Rectangle topmidpocket = {520,20,80,80};
+	Rectangle bottomleftpocket = {20,567,80,80};
+	Rectangle bottomrightpocket = {1040,567,80,80};
+	Rectangle bottommidpocket = {520,567,80,80};
+		
 	while(!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		 render_asset(table);
 		 render_asset(cue);
-		 render_asset(triangle);
+		 
 		 for (int i= 0; i< BALL_COUNT; i++){
 		 	render_asset(balls[i]);
 		 }
+
+		
+		// // walls
+		//  DrawRectangleRec(topleft,BLUE);
+		//  DrawRectangleRec(topright,BLUE);
+		//  DrawRectangleRec(bottomright,BLUE);
+		//  DrawRectangleRec(bottomleft,BLUE);
+		//  DrawRectangleRec(sideleft,BLUE);
+		//  DrawRectangleRec(sideright,BLUE);
+		//  // pockets
+		//  DrawRectangleRec(topleftpocket,RED);
+		//  DrawRectangleRec(toprightpocket,RED);
+		//  DrawRectangleRec(topmidpocket,RED);
+		//  DrawRectangleRec(bottomleftpocket,RED);
+		//  DrawRectangleRec(bottomrightpocket,RED);
+		//  DrawRectangleRec(bottommidpocket,RED);
 		// TODO GAME LOGIC
 		EndDrawing();
 	}
-
+	// cleanup 
 	for (int i= 0; i <BALL_COUNT; i++){
 		UnloadTexture(balls[i].texture);
 	}
@@ -66,35 +97,4 @@ int main(){
 	UnloadTexture(triangle.texture);
 	quit();
 	return 0;
-}
-
-// =============================FUNCTION Implementation ======================================
-
-
-void init(){
-	InitWindow(width, height, "8ball pool game");
-		SetTargetFPS(60);
-}
-void quit(){
-	CloseWindow();
-	// TODO unload assets
-}
-
-Asset load_asset(char * file_path, int x, int y, float scale){
-	Asset new_asset;
-	new_asset.file_path = file_path;
-	new_asset.x = x;
-	new_asset.y= y;
-	new_asset.scale = scale;
-	new_asset.texture= LoadTexture(file_path);
-	return new_asset;
-}
-
-void render_asset(Asset new_asset){
-	float scale = new_asset.scale;
-	Rectangle src = {0,0, new_asset.texture.width,new_asset.texture.height};
-	Rectangle dst = {new_asset.x,new_asset.y, new_asset.texture.width * scale,new_asset.texture.height*scale};
-	Vector2 origin ={0,0};
-	double rotation = 0.0f;
-	DrawTexturePro(new_asset.texture, src, dst, origin, rotation, WHITE);
 }
